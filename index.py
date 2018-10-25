d33924ef60009715990b9f883c8620f97fefeb5f
Jakub Jankiewicz <jcubic@onet.pl>
2018-10-25 17:59:28 +0200

    wyszukiwarka + poprawki css do tagów

---
+++ index.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import os, sys, re, sqlite3
+from bs4 import BeautifulSoup
+
+# pip install beautifulsoup4
+# pip install html5lib
+
+def get_data(html):
+    """return dictionary with title url and content of the blog post"""
+    tree = BeautifulSoup(html, 'html5lib')
+
+    body = tree.body
+    if body is None:
+        return None
+
+    for tag in body.select('script'):
+        tag.decompose()
+    for tag in body.select('style'):
+        tag.decompose()
+    for tag in body.select('figure'):
+        tag.decompose()
+
+    text = tree.findAll("div", {"class": "content"})
+    if len(text) > 0:
+      text = text[0].get_text(separator='\n')
+    else:
+      text = None
+    title = tree.findAll("h2", {"itemprop" : "title"})
+    url = tree.findAll("link", {"rel": "canonical"})
+    if len(title) > 0:
+      title = title[0].get_text()
+    else:
+      title = None
+    if len(url) > 0:
+      url = url[0]['href']
+    else:
+      url = None
+    result = {
+      "title": title,
+      "url": url,
+      "text": text
+    }
+    return result
+
+
+if __name__ == '__main__':
+  if len(sys.argv) == 2:
+    db_file = 'index.db'
+    if os.path.exists(db_file):
+      os.remove(db_file)
+    conn = sqlite3.connect(db_file)
+    c = conn.cursor()
+    c.execute('CREATE TABLE page(title text, url text, content text)')
+    for root, dirs, files in os.walk(sys.argv[1]):
+      for name in files:
+        if name.endswith(".html") and re.search(r"[/\\]20[0-9]{2}", root):
+          fname = os.path.join(root, name)
+          f = open(fname, "r")
+          data = get_data(f.read())
+          f.close()
+          if data is not None:
+            c.execute('INSERT INTO page VALUES(?, ?, ?)', (data['title'], data['url'], data['text']))
+            print "indexed %s" % data['url']
+            sys.stdout.flush()
+    conn.commit()
+    conn.close()
---
+++ search.php
@@ -0,0 +1,35 @@
+---
+layout: default
+---
+<section>
+    <div class="search">
+      <header><h2>Wyszukanie "<?= isset($_GET['q']) ? strip_tags($_GET['q']) : '' ?>"</h2></header>
+<?php
+
+function mark($query, $str) {
+  return preg_replace("%(" . $query . ")%i", '<mark>$1</mark>', $str);
+}
+
+if (isset($_GET['q'])) {
+  $db = new PDO('sqlite:index.db');
+  $stmt = $db->prepare('SELECT * FROM page WHERE content LIKE :var OR title LIKE :var');
+  $wildcarded = '%'. $_GET['q'] .'%';
+  $stmt->bindParam(':var', $wildcarded); // trzeba użyć zmiennej w tym miejscu
+  $stmt->execute();
+  $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  $query = str_replace("%", "\\%", preg_quote($_GET['q']));
+  $re = "%(?>\S+\s*){0,10}(" . $query . ")\s*(?>\S+\s*){0,10}%i";
+
+  foreach ($data as $row) {
+    if (preg_match($re, $row['content'], $match)) {
+      echo '<h3><a href="' . $row['url'] . '">' . mark($query, $row['title']) . '</a></h3>';
+      $text = trim($match[0], " \t\n\r\0\x0B,.{}()-");
+      echo '<p>' . mark($query, $text) . '</p>';
+    }
+  }
+}
+
+?>
+    </div>
+</section>
