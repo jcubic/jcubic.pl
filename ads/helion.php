6cea18e766c78e945163104d4058889cdf3e8f7a
Jakub Jankiewicz <jcubic@onet.pl>
2019-10-17 21:25:50 +0200

    fix cache of php generated js (helion ads)

--- ads/helion.php
+++ ads/helion.php
@@ -12,6 +12,8 @@ function validate_code($code) {
     return implode(",", array_slice($items, 0, 5));
 }
 
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 header('Content-Type: application/javascript; charset=utf-8');
 
 $partner = '12418M';
